d646ed05dfbb740b255ce7182050f458d4a5049f
Pyotr <p.gorbunov@solitionfactory.ru>
2022-02-20 22:51:36 +0700

    add step

---
+++ Assets/LearnMaterials 2/Scripts/GameScripts/StepScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class StepScript : GameScript
+{
+    [SerializeField]
+    private float speed = 1.2f;
+
+    [SerializeField]
+    private GameObject mygameObject;
+
+    [SerializeField]
+    private float step = 1;
+
+    [SerializeField]
+    private float count = 1;
+
+    [SerializeField]
+    private Vector3 spawn_point;
+
+    public override void Use() 
+    {
+        Debug.Log("test3");
+        StartCoroutine(Step());
+    }
+
+    private IEnumerator Step()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            spawn_point.x += step;
+            Instantiate(mygameObject, spawn_point, Quaternion.identity);
+            yield return null;
+
+        }
+        
+    }
+}
