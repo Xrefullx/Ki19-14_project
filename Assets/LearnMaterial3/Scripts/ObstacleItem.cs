a18afb8f84a8ca38daa4475a7feb882491454dc1
CoookieMaster <thegirlinthe_hat@mail.ru>
2022-03-16 21:09:44 +0700

    1 скрипт

--- Assets/LearnMaterial3/Scripts/ObstacleItem.cs
+++ Assets/LearnMaterial3/Scripts/ObstacleItem.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ObstacleItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float currentValue ;
+    public UnityEvent onDestroyObstacle;
+    public MeshRenderer mesh;
 
-    // Update is called once per frame
-    void Update()
+    void GetDamage(float value)
     {
-        
+        currentValue -= value;
+        if (currentValue == 1) {
+            mesh.materials[0].color = Color.Lerp(mesh.materials[0].color, Color.white, value);
+        } else if (currentValue == 0)
+        {
+            mesh.materials[0].color = Color.Lerp(mesh.materials[0].color, Color.red, value);
+            onDestroyObstacle.Invoke();
+            Destroy(gameObject);
+        }
     }
+    /*private IEnumerator ChangeColor()
+    {
+        Vector3 start = myTransform.eulerAngles;
+        float t = 0;
+        Vector3 angle = new Vector3(0, 90, 0);
+        while (t < 9)
+        {
+            t += Time.deltaTime * speed;
+            myTransform.localRotation = Quaternion.Euler(Vector3.Lerp(start, angle, t));
+            yield return null;
+        }
+    }*/
 }
